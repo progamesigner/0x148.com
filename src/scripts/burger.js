7874af2ebb36b1062b957e99c2b024f9fafaa646
Yang Sheng Han <progamesigner@gmail.com>
2019-04-11 13:38:15 +0800

    Move scripts and styles to standalone theme

--- src/scripts/burger.js
+++
@@ -1,6 +0,0 @@
-export default element => {
-    element.addEventListener('click', () => {
-        element.classList.toggle('is-active')
-        element.parentNode.parentNode.querySelector('.navbar-menu').classList.toggle('is-active')
-    })
-}
